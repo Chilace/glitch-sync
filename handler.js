750ed6578c2148770043498e624cea8856202f19
Chilace <22879802+Chilace@users.noreply.github.com>
2019-01-14 06:16:14 +0300

    Refactor handler

--- handler.js
+++ handler.js
@@ -14,39 +14,45 @@ module.exports = (req, res) => {
     res.end(err.toString());
   }
 
-  if (!/application\/json/.test(contentType)) return onError(new Error('Unexpected Content-Type'));
-  if (!userAgent.startsWith('GitHub-Hookshot')) return onError(new Error('Unexpected User-Agent'));
   req.on('error', onError);
-  req.on('data', chunk => json += chunk);
-  req.on('end', () => {
-    const hash = crypto.createHmac('sha1', secret).update(json).digest('hex');
-    if (hash !== signature) return onError(new Error('Invalid HMAC'));
-    try {
-      body = JSON.parse(json);
-    } catch (err) {
-      return onError(err);
-    }
-    console.log(`\nRequest is a "${event}" webhook, Repo: "${body.repository.full_name}"`);
-
-    if (event === 'push' && body.ref === 'refs/heads/master') {
-      console.log('\nSync...');
-      const gitUrl = body.repository.git_url;
-      exec(`git pull -X theirs ${gitUrl} ${isGlitch ? '&& refresh' : ''}`, (err, stdout, stderr) => {
-        if (err) {
-          console.error(`\n${err}\n\n==========\n`);
-          fs.appendFileSync('./push_log.txt', `\n${new Date()}\n${err}\n\n==========\n`);
-          res.statusCode = 500;
-          return res.end(err.toString());
+  if (!/application\/json/.test(contentType)) onError(new Error('Unexpected Content-Type'));
+  else if (!userAgent.startsWith('GitHub-Hookshot')) onError(new Error('Unexpected User-Agent'));
+  else {
+    req.on('data', chunk => json += chunk);
+    req.on('end', () => {
+      const hash = crypto.createHmac('sha1', secret).update(json).digest('hex');
+      if (hash !== signature) onError(new Error('Invalid HMAC'));
+      else {
+        try {
+          body = JSON.parse(json);
+        } catch (err) {
+          onError(err);
+          return;
         }
+        console.log(`\nRequest is a "${event}" webhook, Repo: "${body.repository.full_name}"`);
 
-        console.log(`\n${stderr}`);
-        console.log(`\nResult: ${stdout}\n\n==========\n`);
-        fs.appendFileSync('./push_log.txt', `\n${new Date()}\nResult: ${stdout}\n\n==========\n`);
-        res.end(stdout);
-      });
-    } else {
-      console.log('\nNot a push to master!');
-      res.end('OK');
-    }
-  });
+        if (event === 'push' && body.ref === 'refs/heads/master') {
+          console.log('\nSync...');
+          const gitUrl = body.repository.git_url;
+          exec(`git pull -X theirs ${gitUrl} ${isGlitch ? '&& refresh' : ''}`, (err, stdout, stderr) => {
+            if (err) {
+              console.error(`\n${err}\n\n==========\n`);
+              fs.appendFileSync('./push_log.txt', `\n${new Date()}\n${err}\n\n==========\n`);
+              res.statusCode = 500;
+              res.end(err.toString());
+              return;
+            }
+
+            console.log(`\n${stderr}`);
+            console.log(`\nResult: ${stdout}\n\n==========\n`);
+            fs.appendFileSync('./push_log.txt', `\n${new Date()}\nResult: ${stdout}\n\n==========\n`);
+            res.end(stdout);
+          });
+        } else {
+          console.log('\nNot a push to master!');
+          res.end('OK');
+        }
+      }
+    });
+  }
 };
